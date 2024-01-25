6536b6d2a8fe1b75b863af72be715ff302873709
aakumar850 <44788305+aakumar850@users.noreply.github.com>
2024-01-25 19:46:45 +0000

    updated completions to anthropic 2.1

--- pages/5_Anthropic.py
+++ pages/5_Anthropic.py
@@ -2,10 +2,11 @@ from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 import streamlit as st
 anthropic = Anthropic()
 anthropic.api_key=st.secrets['ANTHROPIC_API_KEY']
-prompt = st.text_input('Ask your question', 'How to save money?')
+prompt = st.text_input('Ask your question', '?')
 completion = anthropic.completions.create(
     model="claude-2.1",
-    max_tokens_to_sample=300,
-   prompt=f"{HUMAN_PROMPT} {prompt} {AI_PROMPT}",
+    max_tokens_to_sample=1000,
+    temperature=0,
+    prompt=f"{HUMAN_PROMPT}{AI_PROMPT}",
 )
 st.write(completion.completion)
