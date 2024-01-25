6d8d2d484afbc2d899b85ffe351355212b8cbdf3
aakumar850 <44788305+aakumar850@users.noreply.github.com>
2024-01-25 18:54:57 +0000

    updated model to 2.1

--- pages/5_Anthropic.py
+++ pages/5_Anthropic.py
@@ -4,7 +4,7 @@ anthropic = Anthropic()
 anthropic.api_key=st.secrets['ANTHROPIC_API_KEY']
 prompt = st.text_input('Ask your question', 'How to save money?')
 completion = anthropic.completions.create(
-    model="claude-2",
+    model="claude-2.1",
     max_tokens_to_sample=300,
    prompt=f"{HUMAN_PROMPT} {prompt} {AI_PROMPT}",
 )
